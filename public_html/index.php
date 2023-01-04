79af42b187ba123512248b158b93186839e2cd6c
tornttwt <tornttwt@gmail.com>
2023-01-04 17:37:45 +0700

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,47 +1,47 @@
 <form method="post">
-    <input name="start" type="text"
+    Fist Number : <input name="start" type="text"
     value="<?php echo isset($_POST['start'])
         ? $_POST['start'] : "" ; ?>">
-    <button type="submit">Submit</button>
-</form>
-
-<form method="post">
-    <input name="end" type="text"
+    &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+    Last Number : <input name="end" type="text"
     value="<?php echo isset($_POST['end'])
         ? $_POST['end'] : "" ; ?>">
     <button type="submit">Submit</button>
 </form>
 
+
 <table>
     <?php
     $start = isset($_POST['start'])
         ? intval($_POST['start'])
-        : 2;
+        : 0;
     $end = isset($_POST['end'])
         ? intval($_POST['end'])
-        : 2;
+        : 0;
     ?>
-    <?php print "Even numbers are"?>
-    <?php
-    for($i = $start ; $i <= $end ; $i++){ ?>}
+    <tr>
+        <?php print "Odd numbers are"?>
+        <?php
+        for($i = $start ; $i <= $end ; $i++){ ?>
 
-    <?php
-    if ($i % 2 == 0){
-        echo $i;
-    }
-    ?>
-    <?php } ?>
-    
-    <?php print "Even numbers are"?>
-    <?php
-    for($i = $start ; $i <= $end ; $i++){ ?>}
+            <?php
+            if ($i % 2 != 0){
+                echo $i;
+            }
+            ?>
+        <?php } ?>
+    </tr>
+    <?php echo "<br>" ?>
+    <tr>
+        <?php print "Even numbers are"?>
+        <?php
+        for($i = $start ; $i <= $end ; $i++){ ?>
 
-    <?php
-    if ($i % 2 != 0){
-        echo $i;
-    }
-    ?>
-    
-
-<?php } ?>
+            <?php
+            if ($i % 2 == 0){
+                echo $i;
+            }
+            ?>
+        <?php } ?>
+    </tr>
 </table>
